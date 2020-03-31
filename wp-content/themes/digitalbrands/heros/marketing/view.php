ee1dd077b5ceaf123e22e5ca98b2766320b55a9b
Larry Stanfield <vince.omega@gmail.com>
2020-03-31 16:01:36 -0400

    Setting up hero for marketing site.

---
+++ wp-content/themes/digitalbrands/heros/marketing/view.php
@@ -0,0 +1,77 @@
+<header class="header header-desktop marketing masthead text-white text-center d-flex d-lg-flex d-md-none d-sm-none" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+
+    <div class="row">
+
+        <section class="col-12 mx-auto">
+            <h1 class="hero-header"> 
+                <div class="hero-header-first"><span class="highlight-blue">ELEVATING</span> BUSINESSES THROUGH </div> 
+                <div class="hero-header-second">HIGH QUALITY DIGITAL <span class="highlight-blue">BRANDING:</span></div>
+                <div class="hero-header-cta">
+                    <button type="button" class="btn btn-primary capslock">Get a free consultation</button>
+                </div>
+            </h1>
+        </section>
+
+        <!--
+        <aside class="header-icons">
+
+        </aside>
+
+        <aside class="header-cta">
+            <button class="btn btn-primary">
+        </aside>
+        -->
+
+    </div>
+
+</header>
+
+
+<header class="header header-tablet marketing masthead text-white text-center d-flex d-lg-none d-md-flex d-sm-none" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+
+    <div class="row">
+
+        <section class="col-12 mx-auto">
+            <h1 class="hero-header"> 
+                <div class="hero-header-first"><span class="highlight-blue">ELEVATING</span> BUSINESSES THROUGH </div> 
+                <div class="hero-header-second">HIGH QUALITY DIGITAL <span class="highlight-blue">BRANDING:</span></div>
+                <div class="hero-header-cta">
+                    <button type="button" class="btn btn-primary capslock">Get a free consultation</button>
+                </div>
+            </h1>
+        </section>
+
+        <!--
+        <aside class="header-icons">
+
+        </aside>
+
+        <aside class="header-cta">
+            <button class="btn btn-primary">
+        </aside>
+        -->
+
+    </div>
+
+</header>
+
+
+<header class="header header-mobile marketing masthead text-white text-center d-flex d-lg-none d-md-none d-sm-flex" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+
+    <div class="row">
+
+        <section class="col-12 mx-auto">
+            <h1 class="hero-header"> 
+                <div class="hero-header-first">SUPERIOR WEBSITES</div> 
+            </h1>
+        </section>
+
+        <section class="col-12 mx-auto">
+            <div class="header-cta">
+                <button type="button" class="btn btn-gradient-blue-purple capslock text-white">GET A FREE CONSULTATION</button>
+            </div>
+        </section>
+
+    </div>
+
+</header>
