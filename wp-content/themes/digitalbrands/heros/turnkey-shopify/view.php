da2afd595e846f15235730de823bb8f764e1ac2a
Larry Stanfield <vince.omega@gmail.com>
2020-04-03 21:33:50 -0400

    shopify hero done

--- wp-content/themes/digitalbrands/heros/turnkey-shopify/view.php
+++ wp-content/themes/digitalbrands/heros/turnkey-shopify/view.php
@@ -62,13 +62,22 @@
 
 		<section class="col-12 mx-auto">
 			<h1 class="hero-header"> 
-				<div class="hero-header-first">SUPERIOR WEBSITES</div> 
+				<div class="hero-header-first">CONGRATULATIONS!</div> 
 			</h1>
+			<h2 class="wt-300 hero-secondary-header capslock text-white">selling on shopify is the easiest way</h2>
+			<h2 class="wt-300 hero-secondary-header capslock text-white">to receive a second income</h2>
 		</section>
 
 		<section class="col-12 mx-auto">
 			<div class="header-cta">
 				<button type="button" class="btn btn-gradient-blue-purple capslock text-white">GET A FREE CONSULTATION</button>
+			</div>
+		</section>
+
+		<section class="col-12 mx-auto text-under-cta">
+			<div>
+				<h2 class="wt-300 text-under-cta-secondary-header capslock text-white">GUARANTEED</h2>
+				<h2 class="wt-300 text-under-cta-secondary-header capslock text-white">14 DAY TURNAROUND</h2>
 			</div>
 		</section>
 
