7774bff54613ee2404ee70d33b6ae3c867a02bbe
Larry Stanfield <vince.omega@gmail.com>
2020-04-08 16:05:58 -0400

    adds logos to the black bars.

--- wp-content/themes/digitalbrands/page.php
+++ wp-content/themes/digitalbrands/page.php
@@ -24,12 +24,17 @@
   <section class="features-icons text-center rounded-black">
     <div class="container">
       <div class="row">
-        <div class="col-lg-12">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
+        <div class="col-12">
+          <div class="features-icons-item">
+            <div class="features-icons-icon">
 				<ul>
-					  <li> <i class="icon-screen-desktop m-auto text-primary"></i> </li>
-					  
+					  <li> <img src="<?php echo get_template_directory_uri(); ?>/library/images/logos/android.png"> </li>
+					  <li> <img src="<?php echo get_template_directory_uri(); ?>/library/images/logos/apple.png"> </li>
+					  <li> <img src="<?php echo get_template_directory_uri(); ?>/library/images/logos/amazon.png"> </li>
+					  <li> <img src="<?php echo get_template_directory_uri(); ?>/library/images/logos/instagram.png"> </li>
+					  <li> <img src="<?php echo get_template_directory_uri(); ?>/library/images/logos/wordpress.png"> </li>
+					  <li> <img src="<?php echo get_template_directory_uri(); ?>/library/images/logos/shopify.png"> </li>
+
 				</ul>
             </div>
           </div>
