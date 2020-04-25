53c10c2fcbc9c5ba180f53fb6c00f8945e52b895
Larry Stanfield <vince.omega@gmail.com>
2020-04-25 19:41:04 -0400

    Rename a lot of files, finally got the work section working as intended. Going to start working on the desktop version and then fix the small issues.

--- wp-content/themes/digitalbrands/sections/work/view.php
+++ wp-content/themes/digitalbrands/sections/work/view.php
@@ -1,3 +1,4 @@
+<!--
 <section class="showcase rounded-black">
 		<div class="container">
 			<h2 class="mb-5">
@@ -14,7 +15,7 @@
 						<img src="<?php echo get_template_directory_uri();?>/library/images/work/dash.png">
 					</section>
 				</div>
-				<div class="showcase-item showcase-item-reversed">
+				<div class="showcase-item">
 					<section class="col-9 column" >
 						<img src="<?php echo get_template_directory_uri();?>/library/images/work/formlabs.png">
 					</section>
@@ -25,7 +26,7 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="showcase-item showcase-item-reversed">
+				<div class="showcase-item">
 					<section class="col-6 column" >
 						<img src="<?php echo get_template_directory_uri();?>/library/images/work/hex.png">
 					</section>
@@ -35,4 +36,89 @@
 				</div>
 			</div>
 		</div>
-	</section>
+</section>
+-->
+<?php
+	
+	$pageInfo = get_page( get_the_ID() );
+
+  	$file   = file_get_contents( get_template_directory()."/sections/work/config.json" );
+  	$json   = json_decode( $file );
+
+  	$pages = file_get_contents( get_template_directory()."/heros/config.json" );
+  	$pagesJsonData = json_decode( $pages );
+
+  	$packagesArray = [];
+
+  	if( !empty( $pagesJsonData->pages->{ $pageInfo->post_name } ) && !is_null(  $pagesJsonData->pages->{ $pageInfo->post_name }  ) ){
+
+    	$pageName     		= $pagesJsonData->pages->{ $pageInfo->post_name };
+    	$workArray  		= $json->pages->{ $pageName };
+    	$workCollection 	= $json->work;
+
+  	}
+
+?>
+
+<section class="showcase rounded-black">
+
+	<div class="container">
+
+		<h2 class="mb-5">
+			<div class="hero-header-first">WORK THAT MAKES US</div> 
+			<div class="hero-header-second"><span class="highlight-blue">HAPPY</span> AND <span class="highlight-blue">PROUD:</span></div>
+		</h2>
+
+		<?php foreach( $workArray as $row ){ ?>
+
+			<div class="row">
+
+				<?php foreach( $row as $item ){ ?>
+
+					<div class="showcase-item">
+
+						<?php if( count( $item ) > 1 ) { ?>
+
+							<?php if( count( $item ) > 1 && count( $item[0] ) > 1 ) { ?>
+
+								<section class="col-3 column">
+
+							<?php } else { ?>
+
+								<section class="col-9 column">
+
+							<?php } ?>
+
+							<?php foreach( $item as $entries ) { ?>
+
+								<?php //var_dump( $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ); die(); ?>
+
+								<img src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>">
+
+							<?php } ?>
+
+							</section>
+						
+						<?php } else { ?>
+
+							<?php foreach( $item as $entries ) { ?>
+
+								<section class="col-lg-6 col-sm-12 column">
+
+									<img src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>">
+
+								</section>
+
+							<?php } ?>
+
+
+						<?php } ?>
+
+					</div>
+
+				<?php } ?>
+
+			</div>
+
+		<?php } ?>
+</section>
