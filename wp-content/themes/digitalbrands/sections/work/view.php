ef31b1c614fbda5339ac0242ae286706c7cff6d2
Larry Stanfield <vince.omega@gmail.com>
2020-05-04 21:05:34 -0400

    fixed most of the desktop view for chrome, testing in other browsers.

--- wp-content/themes/digitalbrands/sections/work/view.php
+++ wp-content/themes/digitalbrands/sections/work/view.php
@@ -1,6 +1,7 @@
 <?php
 	
 	$pageInfo = get_page( get_the_ID() );
+	$item = 1;
 
   	$file   = file_get_contents( get_template_directory()."/sections/work/config.json" );
   	$json   = json_decode( $file );
@@ -29,56 +30,116 @@
 			<div class="hero-header-second"><span class="highlight-blue">HAPPY</span> AND <span class="highlight-blue">PROUD:</span></div>
 		</h2>
 
-		<?php foreach( $workArray as $row ){ ?>
+		<section class="work-desktop d-grid d-lg-grid d-md-grid">
+			<?php foreach( $workArray as $row ){ ?>
 
-			<div class="row">
+				<div class="row">
 
-				<?php foreach( $row as $item ){ ?>
+					<?php foreach( $row as $item ){ ?>
 
-					<div class="showcase-item">
+						<div class="showcase-item item-<?php echo $item; ?>">
 
-						<?php if( count( $item ) > 1 ) { ?>
+							<?php if( count( $item ) > 1 ) { ?>
 
-							<?php if( count( $item ) > 1 && count( $item[0] ) > 1 ) { ?>
+								<?php if( count( $item ) > 1 && count( $item[0] ) > 1 ) { ?>
 
-								<section class="col-3 column">
+									<section class="col-3 column <?php echo $workCollection->{ $entries[0] }->class; ?>">
 
-							<?php } else { ?>
+								<?php } else { ?>
 
-								<section class="col-9 column">
+									<section class="col-9 column <?php echo $workCollection->{ $entries[0] }->class; ?>">
 
-							<?php } ?>
+								<?php } ?>
 
-							<?php foreach( $item as $entries ) { ?>
+								<?php foreach( $item as $entries ) { ?>
 
-								<?php //var_dump( $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ); die(); ?>
-
-								<a target="_blank" href="<?php echo $workCollection->{ $entries[0] }->href; ?>" class="<?php echo $workCollection->{ $entries[0] }->class; ?>"><img class="<?php echo $workCollection->{ $entries[0] }->class; ?>" src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>"></a>
-
-							<?php } ?>
-
-							</section>
-						
-						<?php } else { ?>
-
-							<?php foreach( $item as $entries ) { ?>
-
-								<section class="col-lg-6 col-sm-12 column">
+									<?php //var_dump( $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ); die(); ?>
 
 									<a target="_blank" href="<?php echo $workCollection->{ $entries[0] }->href; ?>" class="<?php echo $workCollection->{ $entries[0] }->class; ?>"><img class="<?php echo $workCollection->{ $entries[0] }->class; ?>" src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>"></a>
 
+								<?php } ?>
+
 								</section>
+							
+							<?php } else { ?>
+
+								<?php foreach( $item as $entries ) { ?>
+
+									<section class="col-lg-6 col-sm-12 column">
+
+										<a target="_blank" href="<?php echo $workCollection->{ $entries[0] }->href; ?>" class="<?php echo $workCollection->{ $entries[0] }->class; ?>"><img class="<?php echo $workCollection->{ $entries[0] }->class; ?>" src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>"></a>
+
+									</section>
+
+								<?php } ?>
+
 
 							<?php } ?>
 
+						</div>
 
-						<?php } ?>
+						<?php $item++; ?>
 
-					</div>
+					<?php } ?>
 
-				<?php } ?>
+				</div>
 
-			</div>
+			<?php } ?>
+		</section>
 
-		<?php } ?>
+		<section class="work-mobile d-grid d-lg-none d-md-none d-sm-grid">
+			<?php foreach( $workArray as $row ){ ?>
+
+				<div class="row">
+
+					<?php foreach( $row as $item ){ ?>
+
+						<div class="showcase-item">
+
+							<?php if( count( $item ) > 1 ) { ?>
+
+								<?php if( count( $item ) > 1 && count( $item[0] ) > 1 ) { ?>
+
+									<section class="col-3 column">
+
+								<?php } else { ?>
+
+									<section class="col-9 column">
+
+								<?php } ?>
+
+								<?php foreach( $item as $entries ) { ?>
+
+									<?php //var_dump( $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ); die(); ?>
+
+									<a target="_blank" href="<?php echo $workCollection->{ $entries[0] }->href; ?>" class="<?php echo $workCollection->{ $entries[0] }->class; ?>"><img class="<?php echo $workCollection->{ $entries[0] }->class; ?>" src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>"></a>
+
+								<?php } ?>
+
+								</section>
+							
+							<?php } else { ?>
+
+								<?php foreach( $item as $entries ) { ?>
+
+									<section class="col-lg-6 col-sm-12 column">
+
+										<a target="_blank" href="<?php echo $workCollection->{ $entries[0] }->href; ?>" class="<?php echo $workCollection->{ $entries[0] }->class; ?>"><img class="<?php echo $workCollection->{ $entries[0] }->class; ?>" src="<?php echo get_template_directory_uri();?>/library/images/work/<?php echo $workCollection->{ $entries[0] }->{ $entries[1]->{ $entries[0] } } ;?>"></a>
+
+									</section>
+
+								<?php } ?>
+
+
+							<?php } ?>
+
+						</div>
+
+					<?php } ?>
+
+				</div>
+
+			<?php } ?>
+		</section>
+
 </section>
