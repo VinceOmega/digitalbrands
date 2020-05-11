e094e8578f5224a05119fcdd3f4c9a1e015d1eaa
Larry Stanfield <vince.omega@gmail.com>
2020-05-11 06:02:22 -0400

    working on heros for tablet view

--- wp-content/themes/digitalbrands/footer.php
+++ wp-content/themes/digitalbrands/footer.php
@@ -2,19 +2,19 @@
 
 				<div class="row">
 
-					<div class="footer-digital-brands col-lg-4 col-md-4 col-sm-12">
+					<div class="footer-digital-brands col-lg-4 col-md-12 col-sm-12">
 						<div class="footer-flavor-text">
 							<h3 class="header-first-word">Digital<strong class="header-second-word">Brands</strong><span class="supascript">&copy;</span></h3>
 							<p>We are a team of creative thinkers and problem solvers dedicated to expanding the limits of what is possible by helping brands achieve their goals. At Digital Brands, we utilize our breadth of experience working with major corporations to fuel creative innovation that delivers real results to companies large and small.</p>
 
-							<div class="col-md copy-right2 d-sm-block d-md-none" align="center">
+							<div class="col-md copy-right2 d-sm-block d-md-block d-lg-none" align="center">
 								<a href="./terms-condition.php"> Terms Of Use </a> and <a href="./privacy-policy.php">Privacy Policy</a> Cookie Policy
 								
 							</div>
 
 							<br><br>
-							
-							<div class="icon-footer d-sm-block d-md-none" align="center" style="padding-bottom: 100px;">
+
+							<div class="icon-footer d-sm-block d-md-block d-lg-none" align="center" style="padding-bottom: 100px;">
 
 								<a href="mailto: hello@digitalbrandsinc.com">
 								<i class="fa fa-envelope-o" style="font-size: 20px; margin-left: 10px;"></i></a>
@@ -37,7 +37,7 @@
 
 					</div>
 
-					<div class="footer-social-media col-lg-4 col-md-3 sm-hide">
+					<div class="footer-social-media col-lg-4 d-md-none d-sm-none">
 
 						<div class="icon-footer" align="center">
 
@@ -60,7 +60,7 @@
 
 					</div>
 
-					<div class="footer-terms-policy col-lg-4 col-md-3 sm-hide">
+					<div class="footer-terms-policy col-lg-4 d-md-none d-sm-none">
 
 						<div class="col-md copy-right2">
 							<p> <a href="./terms-condition.php"> Terms Of Use </a> and <a href="./privacy-policy.php">Privacy Policy</a> Cookie Policy</p>
